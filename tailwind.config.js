15f9704e044b0da21f6acd0a7d34365c6eee2c16
Michael Mazzone <mazzone.deca@gmail.com>
2021-06-04 09:30:11 -0400

    fix(storybook): attempt to fix tailwind not working with storybook
    Not currently working. Tailwind works in app, but doesnt show in storybook.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@ module.exports = {
   purge: [],
   darkMode: false,
   theme: {
-    extend: {},
+    // TEST COLORS TO SEE IF TAILWIND IS WORKING
+    // Example: className="text-primary-dark" or "bg-secondary-light"
+    colors: {
+      primary: {
+        light: '#FACC15',
+        dark: '#EAB308',
+      },
+      secondary: {
+        light: '#854D0E',
+        dark: '#713F12',
+      },
+      accent: {
+        light: '',
+        dark: '',
+      },
+    },
   },
   variants: {
     extend: {},
